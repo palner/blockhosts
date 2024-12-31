735d896b2f7b51ae45d08fc9495282468e8852f4
Fred Posner <fred@pgpx.io>
2024-12-30 22:17:03 -0500

    update watchlist

--- blockhosts.go
+++ blockhosts.go
@@ -358,6 +358,7 @@ func SshAuthCheck(logfile string) ([]string, int, error) {
 	matchRules = append(matchRules, `Connection closed by\D+([0-9]{0,3}\.){3}[0-9]{0,3}`)
 	matchRules = append(matchRules, `Received disconnect from\D+([0-9]{0,3}\.){3}[0-9]{0,3}(.*)\:\s\s\[preauth\]`)
 	matchRules = append(matchRules, `Received disconnect from\D+([0-9]{0,3}\.){3}[0-9]{0,3}(.*)\[preauth\]`)
+	matchRules = append(matchRules, `Connection reset by\D+([0-9]{0,3}\.){3}[0-9]{0,3}(.*)\[preauth\]`)
 	matchRules = append(matchRules, `authentication failure(.*)rhost\=([0-9]{0,3}\.){3}[0-9]{0,3}`)
 	matchRules = append(matchRules, `Failed password for(.*)([0-9]{0,3}\.){3}[0-9]{0,3}`)
 	matchRules = append(matchRules, `Invalid user(.*)([0-9]{0,3}\.){3}[0-9]{0,3}`)
